97c8f605bd777b912b45de410d4306c6ad76eee5
Chris Rice <chrisrice93@gmail.com>
2015-10-05 09:42:18 -0600

    Added new features.

--- backup.rb
+++ backup.rb
@@ -1,4 +1,7 @@
+#!/usr/bin/ruby
+
 require 'yaml'
+require 'fileutils'
 
 class YAMLLoader
     attr_reader :parsed
@@ -16,16 +19,41 @@ class BackerUpper
     attr_reader :syncer_command, :source_directory, :backup_directory
 
     def initialize(args)
-        @syncer_command = args["syncer_command"]
+        @syncer_command = default_syncer_command
         @source_directory = args["source_directory"]
         @backup_directory = args["backup_directory"]
     end
+    
+    def default_syncer_command
+        "rsync -av"
+    end
 
     def backup
-        fork {
-            exec "#{syncer_command} #{source_directory} #{backup_directory}"
-        }
-        puts ""
+        create_backup_directory
+        run_backup_command
+        write_timestamp_to_backup_directory
+    end
+
+    def create_backup_directory
+        FileUtils.mkdir_p(backup_directory)
+    end
+    
+    def run_backup_command
+        system "#{syncer_command} #{source_directory} #{backup_directory}"
+    end
+
+    def write_timestamp_to_backup_directory
+        target = open(timestamp_filename, 'w')
+        target.write(timestamp)
+        target.close 
+    end
+    
+    def timestamp_filename
+        File.join(backup_directory, 'last_backup.txt')
+    end
+
+    def timestamp
+        "#{Time.now}\n"
     end
 end
 
